acbada3ba283a022fe96066f0d51f9ff3edf7090
niuzhist <zhangzhi1986@gmail.com>
2014-11-18 13:31:24 -0500

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -2,7 +2,17 @@ describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
-
+    var cloneObj = function (source, target) {
+        var item;
+        for (item in source) {
+            if (source.hasOwnProperty(item)) {
+                target[item] = source[item];
+            }
+        }
+    }
+	
+	  cloneObj(expected, obj);
+	
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
