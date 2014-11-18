d0b935fd21898688ab4b9f5ea4e8de888c16ac4f
niuzhist <zhangzhi1986@gmail.com>
2014-11-18 13:32:03 -0500

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,6 +2,17 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+        
+    flattenArray = function (arr) {
+      return flattenInner(arr).sort();
+    },
+
+    flattenInner = function (arr) {
+        arr = Array.prototype.concat.apply([], arr);
+        return arr.some(Array.isArray) ? flattenInner(arr) : arr;
+    };	
+	
+	  arr = flattenArray(arr);    
 
     expect(arr).toEqual(expected);
   });
